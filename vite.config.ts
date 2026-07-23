9134d0d3cb032ee6de5a9e45c99d88ae90d45f04
zainaleinstein1982 <88931669+zainaleinstein1982@users.noreply.github.com>
2026-07-23 08:46:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/generatif-3d-meeting-app/',
   plugins: [react()],
   resolve: {
     alias: {
